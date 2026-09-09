162bdeeeac3c5a04181533c5867e93399e08d479
Carlos V.M Lino <wenikius@gmail.com>
2026-09-09 17:43:56 -0300

    V0.1: project start

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace IntuitiveMedia.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Views/PlayerView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace IntuitiveMedia.Views;
+
+// Todo axaml separado precisa de uma classe dessas.
+public partial class PlayerView : UserControl
+{
+    public PlayerView()
+    {
+        InitializeComponent();
+    }
+}
