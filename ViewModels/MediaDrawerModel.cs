38096da870bd0fe5fe1781feba046ff83e9149a0
Carlos V.M Lino <wenikius@gmail.com>
2026-09-16 11:24:55 -0300

    V0.7 (update): bandeja de vídeos separado em arquivo próprio. Seu data-context deve ser o mesmo do PlayerView, no caso, MediaDrawerModel. O estado da interface deste menu de bandeja deve responder aos estados do modelo MediaDrawerModel. PlayerView também, de modo que se o usuário, por exemplo, clicar num vídeo da bandeja, o contexto dos dados deve registrar isso para que PlayerView responda a isto, e assim por diante.

--- ViewModels/MediaDrawerModel.cs
+++ ViewModels/MediaDrawerModel.cs
@@ -1,6 +1,3 @@
-using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-
 namespace IntuitiveMedia.ViewModels;
 
 // TODO: O drawer que contém os videos deve ser um componente separado com seu próprio Data Context.
---
+++ Views/MediaDrawer.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace IntuitiveMedia.Views;
+
+public partial class MediaDrawer : UserControl
+{
+    public MediaDrawer()
+    {
+        InitializeComponent();
+    }
+}
